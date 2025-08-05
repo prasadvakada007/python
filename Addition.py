e845c159fd72a8eaad440cd660f8c13f9f37e9b2
prasadvakada007 <prasadvakada2@gmail.com>
2025-08-05 13:59:28 +0530

    add some lines

--- Addition.py
+++ Addition.py
@@ -2,4 +2,6 @@
 num1 = 10
 num2 = 5
 sum_result = num1 + num2
+sub_result = num1-num2
 print("The sum of", num1, "and", num2, "is:", sum_result)
+print("The sub of ", num1, "and", num2, "is:", sub_result)
